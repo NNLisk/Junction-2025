b184765a8d8c505479657c32c384fbad65a4ac08
NNLisko <niko.lausto@student.lut.fi>
2025-11-15 04:16:29 +0200

    database

--- tauriAppDemo/src-tauri/src/lib.rs
+++ tauriAppDemo/src-tauri/src/lib.rs
@@ -3,6 +3,7 @@ pub mod database;
 pub use database::{}
 use rusqlite::{params, Connection}
 
+pub struct DbConnection(pub Mutex<Connection>);
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -10,17 +11,23 @@ fn greet(name: &str) -> String {
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
-pub fn run(conn: &Connection) {
+pub fn run() {
+
+    let conn = database::initialize_database()
+        .except("Failed to connect to a database");
+
     tauri::Builder::default()
+        .manage(DbConnection(Mutex::new(conn)))
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+
 }
 
 #[tauri::command]
 fn load_data() {
-    // Placeholder for data loading logic
+    database::load_all(&conn).map_err(|e| e.to_string())
 }
 
 #[tauri::command]
--- tauriAppDemo/src-tauri/src/main.rs
+++ tauriAppDemo/src-tauri/src/main.rs
@@ -1,11 +1,8 @@
-pub mod database;
-pub use database::{initialize_database}
-
 
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
     let conn = initialize_database().map_err(|e| e.to_string())?;
-    tauriappdemo_lib::run(&conn)
+    tauriappdemo_lib::run()
 }
