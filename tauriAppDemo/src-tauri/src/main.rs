6228c9f83f72291dc1127195e9a0be174ae06e0d
EmilijaKur <emilija.kurtinaityte@gmail.com>
2025-11-15 11:31:31 +0200

    haha

--- tauriAppDemo/src-tauri/src/main.rs
+++ tauriAppDemo/src-tauri/src/main.rs
@@ -1,8 +1,8 @@
 
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
-/*#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    let conn = initialize_database().map_err(|e| e.to_string())?;
     tauriappdemo_lib::run()
 }
-*/
