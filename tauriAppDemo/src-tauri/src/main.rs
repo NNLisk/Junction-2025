f33b635d1ff4d7920217d5ced0df5f7e7493f952
EmilijaKur <emilija.kurtinaityte@gmail.com>
2025-11-15 11:28:21 +0200

    some ui

--- tauriAppDemo/src-tauri/src/main.rs
+++ tauriAppDemo/src-tauri/src/main.rs
@@ -1,7 +1,8 @@
 
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+/*#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
     tauriappdemo_lib::run()
 }
+*/
